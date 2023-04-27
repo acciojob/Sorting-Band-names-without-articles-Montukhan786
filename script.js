e7c44be4b688239416a602e6697052a282f15f91
Muntjirkhan Pathan <montukhanpathan@gmail.com>
2023-04-27 19:17:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+let updatedList = touristSpots.map( (element) =>{
+	let word = element.split(" ");
+
+	let updateString = word.reduce((prev,current,index) => {
+		if(current != "The" && current != "an" && current != "a"){
+			return prev+" "+current;
+		}
+		return prev;
+	},"")
+
+	return updateString;
+});
+
+console.log(updatedList);
+
+function Sort(updatedList) {
+	return updatedList.sort( (a,b) => a.localCompare(b));
+}
+
+console.log(Sort(updatedList));
+
+
+
+
